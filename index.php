cecdc904f100b3e367ba98fc6976c87429683640
rmac <rmac@rmacs-MBP.home>
2018-01-11 22:00:02 +0000

    file sync with git

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>e m o t i v e</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" href="https://use.typekit.net/lqg0jga.css">
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script src='js/main.js'></script>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no' />
+    <meta charset='UTF-8'>
+    <meta name='description' content='Rheanne McCormick'>
+    <meta name='keywords' content='Rheanne, McCormick, Design, UX, Automotive UX, Development, UAL, University of the Arts, London, Cars, Experience,s Technical Design, Driving, Racing, Petrol Head'>
+    <meta name='author' content='Rheanne McCormick'>
+  </head>
+  <body>
+
+    <div id="main">
+      <h1 id="name">_RM</h1>
+      <p class="intro-text">This site attempts to tell the long and at times frustrating tale of my adventures with cars and computers.</p>
+      <p class="intro-text">keep on scrolling through or jump to a section using the links below.<br/><br/>As you were...</p>
+      <ul id="nav">
+        <li>*a bit about me*</li>
+        <li>*my mission statement*</li>
+        <li>*a series of failures*</li>
+      </ul>
+
+      <!--<p class="intro-text" style="margin-bottom: -50px; padding-bottom: 0px;">project:</p>-->
+      <h1 class="title" style="margin-left: -10px;">EMOTIVE</h1>
+      <img class="wide-img" src="images/header.jpg">
+      <p class="caption">the annual Low Collective auto show, Suffolk</p>
+
+      <!-- a bit about me-->
+
+      <h2 class="sub-heading" style="margin-bottom: 0px; padding-bottom: 0px;">A BIT ABOUT ME</h2>
+      <!--<p class="body-text" style="margin-top: -80px; padding-top: 0px; margin-bottom: 60px;">(because that's the only way you're going to understand why this means so much to me)</p>-->
+      <div class="grid-img" style="background-image: url(gifs/helmets.gif);"></div>
+      <p class="caption">safety is a priority. one of many track days at Snetterton with my dad</p>
+
+      <!-- 2 - how I got into cars / my context -->
+
+      <p class="body-text">I have spent my entire life surrounded by cars and people who love driving them.
+      <br/><br/>
+      I grew up in a house where everything from rally cross to F1 would be on the tv on a sunday, while my dad and his friends wondered in and out of our garage in oil covered overalls, tinkering with various cars he and his friends raced. I was brought up on tales of trips spent on the road exploring wherever the cars could take them
+      <br/><br/>
+      When we weren't watching motorsport, hanging out in freezing cold garages, or in the pit lane of a racetrack - we were at huge car shows, where enthusiasts would gather to see everything from rare ferrari's to military tanks.
+      <br/><br/>
+      For us, a family day out was centered around all piling into car, and heading out on the road for as many miles as we could cover before we wanted to kill each other.
+      <br/><br/>
+      Cars have always been an integral part of my life.</p>
+      <div class="grid-img" style="background-image:url(gifs/pitlane.gif);"></div>
+      <p class="caption">the view from the pitlane at rockingham</p>
+
+
+      <p class="body-text">As I got older, I was allowed onto the track as a passenger, and I experienced a way of driving which was completely different to what I was used to on the road.
+      <br/><br/>
+      I watched as skilled drivers coaxed the best out of their cars. And I realised through physical experience that driving isn’t all about efficient power delivery and tight race lines - it’s a two-participant performance.
+      <br/><br/>
+      And when it’s done just right... it’s an out of body experience as driver and car become harmonious.</p>
+
+      <div class="grid-img" style="background-image:url(images/sunset.jpg)"></div>
+      <p class="caption">pulling up to watch the sun set across the ocean with my family</p>
+
+
+      <!-- 1 - intro spiel - what I care about and want to investigate -->
+
+      <h2 class="sub-heading" style="margin-bottom: 0px; padding-bottom: 0px;">CHANGE MUST COME</h2>
+      <p class="body-text" style="margin-top: -80px; padding-top: 0px; margin-bottom: 60px;">what I want to do and why</p>
+      <p class="body-text">I am frustrated by the developments I see in the industry of self-driving cars, with so much focus on A to B transport of people - but no investment made into building the cars of the future for the masses of people whose hearts break at the thought of cars as a culture becoming extinct. Tesla, Wayfinder, Uber - these companies are implementing AV’s the easiest way they can, as cold objects used simply as tools to complete a task. Does anyone care about what we stand to lose?
+      <br/><br/>
+      We are not a niche, we are strong in numbers, on a global scale vary in how we express our automotive creativity but we all stand by the same principles. Passion for the art, freedom to explore, creativity in execution, and customisation to personal taste. AV development is spearheaded by technology companies, it’s become about the most efficient algorithms and the highest safety specifications - but this is only part of the much bigger picture that they’re failing to recognise.</p>
+
+
+      <!-- 5.5 - what I've learned - why car hacking is a good thing -->
+
+      <p class="body-text">Around 4 months ago, opengarages.org, which houses the car hacker’s handbook. I learned that there are ports in the car that let you connect directly to the computer which controls everything from fuel injection timing to door locking. I had never understood before that these individual electronic controls passed through one central computer - I had seen the inner wirings of cars before but wrongly assumed most controls had independent analogue wiring. I have a strong computing background, I am intrigued by how they work on a deep level and the vast range of possible systems that can be built with them, and this discovery gave me a completely new mental model on how cars worked and what I could do with this computer.
+      <br/><br/>
+      The more I read into car computers, ECU’s and OBD2 ports and protocols - I could relate my experience with desktop computers to this new system and this gave me a lens that I used to pick out what was within my ability to do. I am not an electrician, I don’t feel comfortable touching any hardwired components - especially when any of them could give you a significant electric shock or damage the vehicle. However, the concept of plugging my own computer into a small port under the steering wheel, specifically designed for diagnostics, was realistic. OBD2 hacking is documented on various places on the internet, but only in small parts. I am yet to discover anyone who has started from nothing and developed a system for car hacking which has been completely documented in a way that can be followed. This was an issue for me, because it meant I had to do lots of individual pieces of research into hardware interfaces and systems which I had never had to use before.</p>
+
+
+
+    </div>
+
+  </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,23 @@
+var images = document.getElementsByTagName('img');
+
+function findAddress(image){
+    console.log("finding images")
+    console.log(images)
+    for(i=0;i<images.length; i++){
+       	console.log(images[i].currentSrc)
+    }
+}
+
+var imageAddress = images
+
+var openImage = document.createAttribute("onClick")
+
+
+
+document.addEventListener("click", function(){
+  document.getElementByTagName("img")
+
+
+
+
+})
